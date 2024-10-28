f4a6fa3468bc48862c7cc9f847f56cdd2c2b3f1f
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2024-10-28 16:59:47 +0530

    Update AddItemForm.jsx

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -30,7 +30,7 @@ export default function AddItemForm({ onAddItem }) {
         }}
         autoFocus
       />
-      <Button>Add to list</Button>
+      <Button text = "Add to list"></Button>
     </form>
   );
 }
